b80451d1e89afbe6eb64eb8e757a7103296699c5
lpm2 <Leo@Leos-MacBook-Air-2.local>
2015-12-06 13:07:10 -0600

    Updated autocorrelation py script

--- autocorrelation.py
+++ autocorrelation.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
-import scipy
 import scipy.signal
+#from scipy import *
+#from scipy import signal
+#import scipy.signal
 import numpy
-import csv
 import sys
 
 
@@ -19,20 +20,53 @@ def readData(fileName):
 
 def score(vec):
    
-    vec[numpy.where(vec >= 0.400)] = -5
-    vec[numpy.where((vec >= 0) & (vec < 0.025))] = 5;
-    vec[numpy.where((vec >= 0.025) & (vec < 0.050))] = 4;
-    vec[numpy.where((vec >= 0.050) & (vec < 0.075))] = 3;
-    vec[numpy.where((vec >= 0.075) & (vec < 0.100))] = 2;
-    vec[numpy.where((vec >= 0.100) & (vec < 0.125))] = 1;
-    vec[numpy.where((vec >= 0.125) & (vec < 0.150))] = 0;
-    vec[numpy.where((vec >= 0.150) & (vec < 0.175))] = -1;
-    vec[numpy.where((vec >= 0.175) & (vec < 0.200))] = -2;
-    vec[numpy.where((vec >= 0.200) & (vec < 0.300))] = -3;
-    vec[numpy.where((vec >= 0.300) & (vec < 0.400))] = -4;
-    
+    vec[numpy.where(vec >= 0.400)] = -10
+    vec[numpy.where(vec >= 0.300)] = -9
+    vec[numpy.where(vec >= 0.200)] = -8
+    vec[numpy.where(vec >= 0.175)] = -7
+    vec[numpy.where(vec >= 0.150)] = -6
+    vec[numpy.where(vec >= 0.125)] = -5
+    vec[numpy.where(vec >= 0.100)] = -4
+    vec[numpy.where(vec >= 0.075)] = -3
+    vec[numpy.where(vec >= 0.050)] = -2
+    vec[numpy.where(vec >= 0.025)] = -1
+    vec[numpy.where(vec >= 0)] = -0
+    vec = vec + 5
     
     return vec
+
+
+def removeNoisyData(locs, data):
+    
+    flag = -1
+    
+    while (flag != 0):
+        flag = 0
+        
+        for i in range(0, len(locs)-2):
+            
+            if (data[locs[i]].all() < 0):
+                flag = -1
+                locs[i] = numpy.array([])
+                break
+            
+            if (len(numpy.where(data[locs[i]:locs[i+1]] < 0)[0])):
+                flag = 1
+                
+                if (data[locs[i]] < data[locs[i+1]]):
+                    locs[i] = numpy.array([])
+                else:
+                    locs[i+1] = numpy.array([])
+                break
+            
+            if(not numpy.array_equal(locs[i], numpy.array([1])) and not numpy.array_equal(locs[i], numpy.array([len(data)-1]))):
+                if(sum(data[locs[i]-1:locs[i]+1]).all() < 0):
+                    locs[i] = numpy.array([])
+                    flag = 1
+                    break
+    
+    return locs
+
 
 def seizure(data):
     
@@ -40,40 +74,83 @@ def seizure(data):
     fs = 256
     seiz = 0
     # compute autocorrelation of data
-    adata = scipy.ifft(numpy.multiply(scipy.fft(data), scipy.conj(scipy.fft(data))))
+    adata = scipy.ifft(numpy.multiply(scipy.fft(data, axis = 1), scipy.conj(scipy.fft(data, axis = 1))))
     finalScore = numpy.zeros(len(adata))
     
     for i in range(0, len(adata)-1):
         
         maxIndices = scipy.signal.argrelmax(data[:,i])
         length = numpy.shape(maxIndices)[1]
-        # only one peak, the signal is not periodic
-        if (length < 2):
-            seiz = 0
-            return seiz
         
         maxIndices = numpy.append([1], maxIndices)   # pads with the first and last indices
         maxIndices = numpy.append(maxIndices, [256*6])
         corrSum = numpy.zeros(length)
         
+        maxIndices = removeNoisyData(maxIndices, adata)
+        
+        
+        # only one peak, the signal is not periodic
+        if (length < 2):
+            seiz = 0
+            return seiz
+        
+        
         for k in range(1, length-2):
             
             # the following lines should have a find < 0 instead of > 0, however,
             # there are 0 values that fit that are found when that is the case...
-            timeOne = maxIndices[k] + 1 - numpy.where(numpy.flipud(adata[maxIndices[k-1]:maxIndices[k]]) > 0)[0][0]       #       find(flipud(adata(locs(k-1):locs(k)))<0,1);
-            timeTwo = maxIndices[k] - 1 + numpy.where(adata[maxIndices[k]:maxIndices[k+1]] > 0)[0][0]
+            #timeOne = maxIndices[k] + 1 - numpy.where(numpy.flipud(adata[maxIndices[k-1]:maxIndices[k]]) > 0)[0][0]       #       find(flipud(adata(locs(k-1):locs(k)))<0,1);
+            #timeTwo = maxIndices[k] - 1 + numpy.where(adata[maxIndices[k]:maxIndices[k+1]] > 0)[0][0]
 
-            tempSum = numpy.cumsum(1/fs*adata[timeTwo:timeOne])  
-            corrSum[k] = 1/fs*(timeTwo - 1 + (tempSum > tempSum[-1]/2))[0];
+            #tempSum = numpy.cumsum(1/fs*adata[timeTwo:timeOne])  
+            #corrSum[k] = 1/fs*(timeTwo - 1 + (tempSum > tempSum[-1]/2))[0];
+            
+            
+            d1 = numpy.where(numpy.flipud(adata[maxIndices[k-1]:maxIndices[k]]) < 0)
+            d2 = numpy.where(adata[maxIndices[k]:maxIndices[k+1]] < 0)
+            
+            if(len(d1) == 0):
+                d1 = numpy.array([0])
+                
+            if(len(d2) == 0):
+                d2 = numpy.array([0])
+            
+            timeOne = maxIndices[k] + 1 -d1
+            timeTwo = maxIndices[k] - 1 + d2
+            
+            tempSum = numpy.cumsum(1/fs*adata[timeOne,timeTwo])  
+            
+            # added this in to avoid index out of bounds exception when finding the peak
+            if(len(tempSum) == 0):
+                corrSum[k] = 0
+                break
+            
+            peak = numpy.where(tempSum > tempSum[-1]/2)
+            
+            if(len(numpy.where(tempSum > peak[0]))):
+                corrSum[k] = 0
+                break
+            
+            corrSum[k] = 1/fs*(timeTwo - 1 + peak[0]);
+            
+            # What is the purpose of this break?
+            if k > 4:
+                break
         
         
-        # TODO this line will result in a divide by zero error, take a look at the paper
-        corrSum = corrSum[1:-1]/1#corrSum[0];
+        corrSum[numpy.where(corrSum == 0)] = numpy.array([])
+        
+        if(filter(lambda x: x == 0, corrSum)):
+            return seiz
+        
+        
+        corrSum = corrSum[1:-1]/corrSum[0];
         corrSum = numpy.abs(numpy.round(corrSum) - corrSum)
         results = score(corrSum) 
-        finalScore[i] = numpy.sum(results)
-        
-    return sum(finalScore >= 12) > 0;
+        finalScore[i] = numpy.sum(results) 
+    
+    seiz = sum(finalScore >= 12) > 0;
+    return seiz
 
 
 if __name__ == "__main__":
