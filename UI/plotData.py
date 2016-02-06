d99243f9d7e30bc09b0464a423cf18ef834fd67c
NeuroNates <neuronates@gmail.com>
2016-02-06 11:01:03 -0600

    adding files

---
+++ UI/plotData.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open("DemoEEGData.txt") as f:
+	data = f.read()
+
+data = data.split('\n')
+
+x = [row.split(' ')[0] for row in data]
+y = [row.split(' ')[1] for row in data]
+
+fig = plt.figure()
+
+ax1 = fig.add_subplot(111)
+
+ax1.set_title("EEG Data")
+ax1.set_xlabel('Time (minutes)')
+ax1.set_ylabel('EEG Voltage (mV)')
+
+ax1.plot(x,y, c='r', label='A-O Montage')
+
+leg = ax1.legend()
+
+plt.show()
