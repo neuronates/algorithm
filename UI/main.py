cfb252a47a07fe50cf1081f7cb1e5fc14a0dddea
NeuroNates <neuronates@gmail.com>
2016-02-09 21:47:55 -0600

    create example kv file

---
+++ UI/main.py
@@ -0,0 +1,23 @@
+import kivy
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.app import App
+from kivy.properties import ObjectProperty, StringProperty
+from kivy.uix.boxlayout import BoxLayout
+
+class Controller(BoxLayout):
+
+	label_wid = ObjectProperty()
+	info = StringProperty()
+
+	def do_action(self):
+		self.label_wid.text = 'My label after button press'
+		self.info = 'New info text'
+
+class ControllerApp(App):
+
+	def build(self):
+		return Controller(info='Hello world')
+
+if __name__ == '__main__':
+	ControllerApp().run()
