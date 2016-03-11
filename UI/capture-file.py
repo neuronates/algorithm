92906162da247e56df50ec54fa60d1d9f79e50f8
NeuroNates <neuronates@gmail.com>
2016-03-11 09:43:23 -0600

    Working on integrating spi with UI

--- UI/capture-file.py
+++ UI/capture-file.py
@@ -1,9 +1,26 @@
+from multiprocessing import Process
+#import spiTest
 import picamera
 
 with picamera.PiCamera() as camera:
+	
+	time = 60
+	print 'start'
+	#Initialize concurrent processes
+	p1 = Process(target = camera.wait_recording, args = (time,))
+	print 'before'
+	#p2 = Process(target = execfile("spiTest.py"))
+	print 'after'
 	camera.resolution = (800, 480)
 	camera.framerate = 30
 	camera.start_preview()
 	camera.start_recording('video_demo.h264')
-	camera.wait_recording(60)
+	#camera.wait_recording(60)
+	#Process(target = spiTest).start()
+	#Process(target = camera.wait_recording, args = (time)).start()
+	print 'wait recording'
+	p1.start()
+	print 'done'
+	#p2.start()
 	camera.stop_recording()
+	print 'recording stopped'
--- UI/spiTest.py
+++ UI/spiTest.py
@@ -15,14 +15,15 @@ import time
 import os
 import numpy as np
 
-# Open SPI bus
-spi = spidev.SpiDev()
-spi.open(0,0)
-spi.max_speed_hz = 122000
-spi.mode = 0b01
+if __name__ == '__main__':
+	# Open SPI bus
+	spi = spidev.SpiDev()
+	spi.open(0,0)
+	spi.max_speed_hz = 122000
+	spi.mode = 0b01
 
-# Function to read SPI data from MCP3008 chip
-# Channel must be an integer 0-7
+	# Function to read SPI data from MCP3008 chip
+	# Channel must be an integer 0-7
 def ReadChannel(channel):
   adc = spi.xfer2([1,(8+channel)<<4,0])
   data = ((adc[1]&3) << 8) + adc[2]
