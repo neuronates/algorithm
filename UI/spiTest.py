4898050170b8ac1819a30952244df335c17d3306
lpm2 <lpm2@users.noreply.github.com>
2016-03-11 09:47:33 -0600

    Update spiTest.py

--- UI/spiTest.py
+++ UI/spiTest.py
@@ -24,6 +24,39 @@ if __name__ == '__main__':
 
 	# Function to read SPI data from MCP3008 chip
 	# Channel must be an integer 0-7
+
+  
+	# Define sensor channels
+	chan = [0, 1, 2, 3, 4, 5, 6, 7]
+	sampling_rate = 256
+	window_length = 30
+	num_samples = sampling_rate * window_length
+	precision = 3
+	# Define delay between readings
+	#delay = (1.0/sampling_rate)
+	delay = 1
+
+	eegData = np.empty((sampling_rate * window_length,len(chan)))
+
+	while True:
+	
+		try:
+			for i in xrange(num_samples):
+				eegData[i] = [ConvertVolts(ReadChannel(c), precision) for c in xrange(len(chan))]
+
+	 			# Print out results
+	 			print "--------------------------------------------"  
+	 			print("Voltage : {}V".format(eegData[i]))  
+
+				# Wait before repeating loop
+		 		time.sleep(delay)
+
+		except KeyboardInterrupt:
+			np.savetxt('out2.txt', eegData, delimiter=',')
+			spi.close() 
+			print "Stopped!\n"
+			exit()
+
 def ReadChannel(channel):
   adc = spi.xfer2([1,(8+channel)<<4,0])
   data = ((adc[1]&3) << 8) + adc[2]
@@ -36,34 +69,3 @@ def ConvertVolts(data,places):
   volts = (data * 3.3)/float(1023)#2**24-1)
   volts = round(volts,places)  
   return volts
-  
-# Define sensor channels
-chan = [0, 1, 2, 3, 4, 5, 6, 7]
-sampling_rate = 256
-window_length = 30
-num_samples = sampling_rate * window_length
-precision = 3
-# Define delay between readings
-#delay = (1.0/sampling_rate)
-delay = 1
-
-eegData = np.empty((sampling_rate * window_length,len(chan)))
-
-while True:
-	
-	try:
-		for i in xrange(num_samples):
-			eegData[i] = [ConvertVolts(ReadChannel(c), precision) for c in xrange(len(chan))]
-
- 			# Print out results
- 			print "--------------------------------------------"  
- 			print("Voltage : {}V".format(eegData[i]))  
-
-			# Wait before repeating loop
-	 		time.sleep(delay)
-
-	except KeyboardInterrupt:
-		np.savetxt('out2.txt', eegData, delimiter=',')
-		spi.close() 
-		print "Stopped!"
-		exit()
