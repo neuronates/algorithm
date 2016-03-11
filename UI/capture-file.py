c4cd0a11ac92ba2b02f09c723191d23d4343910d
lpm2 <lpm2@users.noreply.github.com>
2016-03-11 10:11:24 -0600

    Update capture-file.py

--- UI/capture-file.py
+++ UI/capture-file.py
@@ -4,25 +4,31 @@ import picamera
 
 with picamera.PiCamera() as camera:
 	
-	time = 5
-	print 'start'
-	#Initialize concurrent processes
-	p1 = Process(target = camera.wait_recording, args = (time,))
-	print 'before'
-	p2 = Process(target = spiTest.spiTestRun)#execfile("spiTest.py"))
-	print 'after'
-	camera.resolution = (800, 480)
-	camera.framerate = 30
-	camera.start_preview()
-	camera.start_recording('video_demo.h264')
-	p2.start()
-	camera.wait_recording(time)
-	#Process(target = spiTest).start()
-	#Process(target = camera.wait_recording, args = (time)).start()
-	print 'wait recording'
-	#p1.start()
-	print 'done'
-	#p2.start()
-	camera.stop_recording()
-	p2.terminate()
-	print 'recording stopped'
+	try:
+		time = 5
+		print 'start'
+		#Initialize concurrent processes
+		p1 = Process(target = camera.wait_recording, args = (time,))
+		print 'before'
+		p2 = Process(target = spiTest.spiTestRun)#execfile("spiTest.py"))
+		print 'after'
+		camera.resolution = (800, 480)
+		camera.framerate = 30
+		camera.start_preview()
+		camera.start_recording('video_demo.h264')
+		p2.start()
+		camera.wait_recording(time)
+		#Process(target = spiTest).start()
+		#Process(target = camera.wait_recording, args = (time)).start()
+		print 'wait recording'
+		#p1.start()
+		print 'done'
+		#p2.start()
+		camera.stop_recording()
+		p2.terminate()	
+		print 'recording stopped'
+	
+	except KeyboardInterrupt:
+		'Stopped by Keyboard'
+		camera.stop_recording()
+		p2.terminate()	
