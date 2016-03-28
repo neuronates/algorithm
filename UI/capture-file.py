f999fb376d6a547044d01c7f8760695bd251061f
neuronates <neuronates@gmail.com>
2016-03-28 10:10:05 -0500

    add in video title specific for user

--- UI/capture-file.py
+++ UI/capture-file.py
@@ -3,6 +3,14 @@ import spiTest
 import picamera
 from subprocess import call
 
+with open('choosePatient.txt','r') as f:
+    data=f.read()
+name = data.split("\\s+")
+nameh264 = name[0] + ".h264"
+namemp4 = name[0] + ".mp4"
+print nameh264
+print namemp4
+
 with picamera.PiCamera() as camera:
 	
 	try:
@@ -16,7 +24,7 @@ with picamera.PiCamera() as camera:
 		camera.resolution = (800, 480)
 		camera.framerate = 30
 		camera.start_preview()
-		camera.start_recording('video_demo.h264')
+		camera.start_recording(nameh264)
 		p2.start()
 		camera.wait_recording(time)
 		#Process(target = spiTest).start()
@@ -33,5 +41,7 @@ with picamera.PiCamera() as camera:
 		print 'Stopped by Keyboard'
 		camera.stop_recording()
 		p2.terminate()	
+conversion = "\"MP4Box -fps 30 -add " + nameh264 + " " + namemp4 + "\"" 
+print conversion
 convert_video = "MP4Box -fps 30 -add video_demo.h264 video_demo.mp4"
 call([convert_video], shell = True)
