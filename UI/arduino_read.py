2fad6589e5c9154a6da3a3d4c0d6dfe33a22d80b
lpm2 <lpm2@users.noreply.github.com>
2016-03-28 09:37:52 -0500

    Update arduino_read.py
    Reads in a specified number of samples now

--- UI/arduino_read.py
+++ UI/arduino_read.py
@@ -1,7 +1,15 @@
 #! usr/bin/python
 # from http://playground.arduino.cc/interfacing/python
 import serial
+import numpy
 
-ser = serial.Serial('/dev/ttyAMA0', 115200)
-while True:
-	print ser.readline()
+
+def takeSamples(numSamples):
+
+	samples = np.empty(numSamples)
+
+	ser = serial.Serial('/dev/ttyAMA0', 115200)
+	for i in range(numSamples):
+		samples[i] = ser.readline()
+		
+	return samples
