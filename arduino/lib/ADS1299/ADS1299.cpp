987371d280d1dbc2553e7f64125f325b6e7167a3
lpm2 <lpm2@users.noreply.github.com>
2016-03-29 10:26:01 -0500

    Update ADS1299.cpp

--- arduino/lib/ADS1299/ADS1299.cpp
+++ arduino/lib/ADS1299/ADS1299.cpp
@@ -269,7 +269,7 @@ void ADS1299::updateChannelData(){
 		}else{
 			channelData[i] &= 0x00FFFFFF;
 		}
-		Serial.write(channelData[i])
+		Serial.write(channelData[i]);
 		//scale input over voltage range [0V, 3.6V]
 		/*if(i == 0){
 			float voltage = channelData[i];
